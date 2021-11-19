72a84def6cdd4054a9eac5e7f568c34b12b4eb0c
Muhammadali9999 <shahid@webevis.com>
2021-11-19 17:58:57 +0500

    website done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Website",
+    title: "Website-Gatsby",
   },
   plugins: [
     "gatsby-plugin-styled-components",
